cf4357cd20dea634f57da546b39fd1415129f854
Dofu1 <39971331+Dofu1@users.noreply.github.com>
2020-07-01 14:34:46 +0200

    file saving

--- src/CommunicationController.java
+++ src/CommunicationController.java
@@ -11,19 +11,35 @@ public class CommunicationController {
 	public void Send_values() {
 		var buffer = managementController.getBuffer();
 		
-		// iterates through every sensor and measurement time, checks if the data has been logged and outputs it
-		for (int i = 0; i < buffer.length; i++) {
-			for (int j = 0; i < buffer[i].length; i++) {
-				if (buffer[i][j] != null) {
+		BufferedWriter my_writer = null;
+
+		try{
+
+			File my_file = new File("log.txt");
+			if(my_file.createNewFile())System.out.println("File created: " + my_file.getName());
+
+			my_writer = new BufferedWriter(new FileWriter(my_file,true));
+
+			// iterates through every sensor and measurement time, checks if the data has been logged and outputs it
+			for (int i = 0; i < buffer.length; i++) {
+				for (int j = 0; i < buffer[i].length; i++) {
+					
+					if (buffer[i][j] == null) continue;
 					
 					// logs data if it hasn't been before and raises isLogged flag afterwards
 					if (!buffer[i][j].isLogged()) {
-						// Here values would be saved to file or sent
-						System.out.println("Name: " + buffer[i][j].getName() + " | Value: " + buffer[i][j].getValue() + " | Timestamp: " + buffer[i][j].getTimestamp());
+						String log_string="Name: " + buffer[i][j].getName() + " | Value: " + buffer[i][j].getValue() + " | Timestamp: " + buffer[i][j].getTimestamp();
+						System.out.println(log_string);
+						my_writer.write(log_string);
+						my_writer.newLine();
 						buffer[i][j].setLogged(true);
 					}
 				}
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (my_writer != null) my_writer.close();
 		}
 	}
 
